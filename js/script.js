6986820b971d945788a9726c8bb5b5ffbe550829
harsh <109368583+harshsharma1981@users.noreply.github.com>
2022-07-29 20:39:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-//game variables
 let inputdirection = { x: 0, y: 0 };
-const movesound = new Audio("move.wav");
+const movesound = new Audio("move.mp3");
 const foodsound = new Audio("eat.mp3");
-const gameoversound = new Audio("gameover.wav");
+const gameoversound = new Audio("gameover.mp3");
 const musicsound = new Audio("music.mp3");
-let speed = 5;
+let speed = 4;
 let lastpainttime = 0;
-let snakeArray = [{ x: 13, y: 15 }];
-let poisionfood1 = { x: 10, y: 12 };
-let poisionfood2 = { x: 1, y: 4 };
-let poisionfood3 = { x: 11, y: 16 };
-let poisionfood4 = { x: 5, y: 14 };
-let poisionfood5 = { x: 2, y: 12 };
+let snakeArray = [{ x: 13, y: 17 }];
+let poisionfood1 = { x: 2, y: 4 };
+let poisionfood2 = { x: 8, y: 5 };
+let poisionfood3 = { x: 4, y: 10 };
+let poisionfood4 = { x: 15, y: 14 };
+let poisionfood5 = { x: 8, y: 12 };
 let food = { x: 10, y: 10 };
 let score = 0;
+let power = 0;
+// let snakecharmer = {x: 8, y: 1}
 
+// player description
 const name1 = prompt('write your name')
 document.getElementById('result').innerHTML = "Name: " + name1;
 // document.getElementById('result').max;
@@ -22,6 +24,13 @@ if (name1.length > 10) {
   alert("pls write your name in under 10 words reload to write your name")
   document.getElementById('result').style.display = 'none';
 }
+
+// food power
+
+
+
+
+
 //game functions
 
 function main(ctime) {
@@ -43,64 +52,104 @@ function iscollide(snake) {
    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <= 0){
     return true;
    }
+   
+   
    //if snake eat posion food
     if(snake[0].x === poisionfood1.x  &&  snake[0].y === poisionfood1.y){
-     return true;
-  }
-    if(snake[0].x === poisionfood2.x  &&  snake[0].y === poisionfood2.y){
-     return true;
-  }
-    if(snake[0].x === poisionfood3.x  &&  snake[0].y === poisionfood3.y){
-     return true;
-  }
-  // poision food you can uncomment this if required
-    if(snake[0].x === poisionfood4.x  &&  snake[0].y === poisionfood4.y){
-    return true; }
-     if(snake[0].x === poisionfood5.x  &&  snake[0].y === poisionfood5.y){
-     return true;
-  }
   
+        return true;
+    }
+ 
+    if(snake[0].x === poisionfood2.x  &&  snake[0].y === poisionfood2.y){
+      return true;
+    }
+ 
+     if(snake[0].x === poisionfood3.x  &&  snake[0].y === poisionfood3.y){
+      return true;
+    }
+  // poision food you can uncomment this if required
+     if(snake[0].x === poisionfood4.x  &&  snake[0].y === poisionfood4.y){
+      return true;
+    }
+     if(snake[0].x === poisionfood5.x  &&  snake[0].y === poisionfood5.y){
+      return true;
+    }
+  
+     
+ 
 }
 
 function gameEngine() {
   // 1. updating the snakearray and food
+ 
 if(iscollide(snakeArray)){
     gameoversound.play();
     musicsound.pause();
     inputdirection = { x: 0, y: 0 };
     alert('Game Over. press any key to continue');
-    snakeArray = [{ x: 13, y: 15 }];
-    musicsound.play();
+  snakeArray = [{ x: 13, y: 17 }];
+    speed = 5
+  
+
+   
+   
+    // musicsound.play();
+    // musicsound.volume = 0.3;
+  
+  
     score = 0;
-  speed = 5;
+    speed = 6;
+    let a = 1;
+let b = 17;
+let c = 2;
+let d = 16;
+
+food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+poisionfood1 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+poisionfood2 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+ poisionfood3 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+//poision food you can uncomment this if required
+   poisionfood4 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+  poisionfood5 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
 }
 //if snake eaten the food add score and regenerate the food
 if(snakeArray[0].y === food.y && snakeArray[0].x === food.x){
     foodsound.play();
-  speed += 0.5;
     score += 1;
+    speed += 0.5;
+    power += 1;
     if(score>hiscore){
       hiscore = score;
       localStorage.setItem("highScore", JSON.stringify(hiscore));
       highscorebox.innerHTML = "High Score: " + hiscore;
     }
-    
+    if(snakeArray[0].y === food.y && snakeArray[0].x === food.x){
+      foodsound.play();
+      foodsound.volume = 0.3
+      speed += 1;
+    }
     scorenum.innerHTML = "Score: " + score;
 snakeArray.unshift({x: snakeArray[0].x + inputdirection.x, y: snakeArray[0].y + inputdirection.y})
- a = 1;
- b = 17;
- c = 1;
-d = 17;
+ a = 2;
+b = 16;
+ c = 2;
+d = 16;
+
 food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
 poisionfood1 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
 poisionfood2 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
-poisionfood3 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
-// poision food you can uncomment this if required
- poisionfood4 = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
-poisionfood5 = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+ poisionfood3 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+//poision food you can uncomment this if required
+ poisionfood4 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+ poisionfood5 = {x: Math.round(c + (d-c)* Math.random()), y: Math.round(c + (d-c)* Math.random())}
+
+
+
 }
-  
-  if(poisionfood1.y === food.y && poisionfood1.x === food.x){
+
+
+
+if(poisionfood1.y === food.y && poisionfood1.x === food.x){
   a = 1
   b = 17
   food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
@@ -125,6 +174,7 @@ if(poisionfood5.y === food.y && poisionfood5.x === food.x){
   b = 17
   food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
 }
+
 
 
 //moving snake
@@ -170,24 +220,26 @@ snakeArray[0].y  += inputdirection.y;
   poisionfood2Element.classList.add('poisionfood2');
   display.appendChild(poisionfood2Element);
   //3rd poision food
-  poisionfood3Element = document.createElement('div');
-  poisionfood3Element.style.gridRowStart = poisionfood3.y;
-  poisionfood3Element.style.gridColumnStart = poisionfood3.x;
-  poisionfood3Element.classList.add('poisionfood3');
-  display.appendChild(poisionfood3Element);
+   poisionfood3Element = document.createElement('div');
+   poisionfood3Element.style.gridRowStart = poisionfood3.y;
+   poisionfood3Element.style.gridColumnStart = poisionfood3.x;
+ poisionfood3Element.classList.add('poisionfood3');
+   display.appendChild(poisionfood3Element);
   //4rt poision food  poision food you can uncomment this if required
-   poisionfood4Element = document.createElement('div');
-   poisionfood4Element.style.gridRowStart = poisionfood4.y;
-   poisionfood4Element.style.gridColumnStart = poisionfood4.x;
-   poisionfood4Element.classList.add('poisionfood4');
-   display.appendChild(poisionfood4Element);
-  //5th poision food you can uncomment this if required
-   poisionfood5Element = document.createElement('div');
-   poisionfood5Element.style.gridRowStart = poisionfood5.y;
-   poisionfood5Element.style.gridColumnStart = poisionfood5.x;
-  poisionfood5Element.classList.add('poisionfood5');
-   display.appendChild(poisionfood5Element);
-}
+    poisionfood4Element = document.createElement('div');
+    poisionfood4Element.style.gridRowStart = poisionfood4.y;
+    poisionfood4Element.style.gridColumnStart = poisionfood4.x;
+    poisionfood4Element.classList.add('poisionfood4');
+    display.appendChild(poisionfood4Element);
+   //5th poision food you can uncomment this if required
+  poisionfood5Element = document.createElement('div');
+  poisionfood5Element.style.gridRowStart = poisionfood5.y;
+  poisionfood5Element.style.gridColumnStart = poisionfood5.x;
+poisionfood5Element.classList.add('poisionfood5');
+  display.appendChild(poisionfood5Element);
+
+
+
 
 //main logic
 let highscore = localStorage.getItem("highscore");
@@ -199,10 +251,15 @@ else{
   hiscore = JSON.parse(highscore)
   highscorebox.innerHTML = "High Score: " + highscore;
 }
+
+
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
     inputdirection = {x:0, y:1};
+    musicsound.play();
+    musicsound.volume = 0.3;
     movesound.play();
+    movesound.volume = 0.3;
     switch (e.key) {
         case "ArrowUp":
             console.log("Arrowup");
@@ -235,21 +292,39 @@ window.addEventListener('keydown', e =>{
 
 
 function moveup() {
+  musicsound.play();
+  musicsound.volume = 0.3;
+  movesound.play();
+  movesound.volume = 0.3;
   inputdirection.x = 0;
   inputdirection.y = -1;
 }
 
 function movedown() {
+  musicsound.play();
+  musicsound.volume = 0.3;
+  movesound.play();
+  movesound.volume = 0.3;
   inputdirection.x = 0;
   inputdirection.y = 1;
+ 
 }
 
 function moveleft() {
+  musicsound.play();
+  musicsound.volume = 0.3;
+  movesound.play();
+  movesound.volume = 0.3;
   inputdirection.x = -1;
   inputdirection.y = 0;
 }
 
 function moveright() {
+  musicsound.play();
+  musicsound.volume = 0.3;
+  movesound.play();
+  movesound.volume = 0.3;
   inputdirection.x = 1;
   inputdirection.y = 0;
+}
 }
